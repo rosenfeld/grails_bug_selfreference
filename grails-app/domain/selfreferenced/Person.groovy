492a614de72aa316c44f5e0f0381dcc33eee04ce
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2012-04-04 13:59:06 -0300

    create-domain-class Person

---
+++ grails-app/domain/selfreferenced/Person.groovy
@@ -0,0 +1,7 @@
+package selfreferenced
+
+class Person {
+
+    static constraints = {
+    }
+}
---
+++ test/unit/selfreferenced/PersonTests.groovy
@@ -0,0 +1,17 @@
+package selfreferenced
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Person)
+class PersonTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
